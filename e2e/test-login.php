4d01a3485a3e2ef1d409bf16d05e0af293bee4bc
Mag <mag@magwas.rulez.org>
2018-01-21 02:06:56 +0100

    e2e test for join szakkol

--- e2e/test-login.php
+++ e2e/test-login.php
@@ -1,71 +1,25 @@
 <?php //phpcs:disable WordPress.Files.FileName.InvalidClassFileName
 
 require_once 'vendor/autoload.php';
+require_once 'e2e/class-operations.php';
 
-class UserSubscriptionTest extends PHPUnit_Extensions_Selenium2TestCase {
+class UserSubscriptionTest extends Operations {
 
-	public function setUp() {
-		$this->setHost( 'localhost' );
-		$this->setPort( 4444 );
-		$this->setBrowserUrl( 'http://localhost/' );
-		$this->setBrowser( 'firefox' );
+	public function test_join_szakkol() {
+		$this->membership_journey();
+		shell_exec( './tools/wpassure' );
+		$this->join_szakkol();
+		$this->assertException(
+			'PHPUnit_Extensions_Selenium2TestCase_WebDriverException', 'still have join button', function() {
+				$this->byXPath( "//*[@id='ep_dashboard']/button[@class='szakkol-join-button']" );
+			}
+		);
 	}
 
-	public function tearDown() {
-		$this->stop();
-	}
-
-	public function testFormSubmissionWithUsername() {
+	protected function membership_journey() {
 		$this->login();
 		$this->look_at_rules();
-		$this->waitForPageReload(
-			function () {
-				$this->byXPath( "//*[@id='ep_acceptrules']" )->click();
-			}, 10000
-		);
-		$this->dashboard_link = $this->byXPath( "//*[@id='ep_dashboard']/a" );
-		$this->assertEquals( 'http://localhost/hogyan-szerzek-magyar-vagy-emagyar-igazolast', $this->dashboard_link->attribute( 'href' ) );
-	}
-	private function look_at_rules() {
-		$this->url( 'http://localhost/' );
-		$this->dashboard_link = $this->byXPath( "//*[@id='ep_dashboard']/a" );
-		$this->assertEquals( 'http://localhost/alapito-okirat', $this->dashboard_link->attribute( 'href' ) );
-		$this->waitForPageReload(
-			function () {
-				$this->dashboard_link->click();
-			}, 10000
-		);
+		$this->accept_rules();
 	}
 
-
-	private function login() {
-		$this->url( 'http://localhost/wp-login.php' );
-		$this->byId( 'user_login' )->value( 'bob' );
-		$this->byId( 'user_pass' )->value( 'bobpassword' );
-		$this->waitForPageReload(
-			function () {
-				$this->byId( 'loginform' )->submit();
-			}, 10000
-		);
-		$this->assertEquals( 'Howdy, bob', $this->byId( 'wp-admin-bar-my-account' )->text() );
-	}
-
-	function waitForPageReload( $navigate_f, $timeout ) {
-		$id = $this->byCssSelector( 'html' )->getId();
-		call_user_func( $navigate_f );
-		$this->waitUntil(
-			function () use ( $id ) {
-				$html = $this->byCssSelector( 'html' );
-				if ( $html->getId() != $id ) {
-					return true;
-				}
-			}, $timeout
-		);
-	}
-	private function wait_user() {
-		fwrite( STDERR, "\n\nWaiting for you\n\n" );
-		$handle = fopen( 'php://stdin', 'r' );
-		$line   = fgets( $handle );
-		fclose( $handle );
-	}
 }
